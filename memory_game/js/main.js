b82467f36aa045652d1dfdc369525055ab507e96
Charles McQueen <cmcqueen@Charless-MacBook-Pro.local>
2017-06-06 12:20:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,27 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king",];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardTwo = cards[1];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
+
+
+
+
+
+
+
+
+
+
 
